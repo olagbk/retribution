6398071dcde09716ea68d5569d9ab9754b83a235
Aleksandra Gabka <aleksandra.gabka@solarwinds.com>
2019-05-15 22:18:11 +0200

    typescript and webpack setup

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import "phaser";
+type GameConfig = any;
+
+const config: GameConfig = {
+    title: "Starfall",
+    width: 800,
+    height: 600,
+    parent: "game",
+    backgroundColor: "#18216D"
+};
+export class StarfallGame extends Phaser.Game {
+    constructor(config: GameConfig) {
+        super(config);
+    }
+}
+window.onload = () => {
+    var game = new StarfallGame(config);
+};
